f36df054b2d15968f44f4f10aed8952508787785
Sean Dunford <sdunford@mail.usf.edu>
2015-01-18 01:40:14 -0800

    A little reorg

---
+++ BitWatch/UIColor+BitWatch.swift
@@ -0,0 +1,17 @@
+//
+//  UIColor+BitWatch.swift
+//  BitWatch
+//
+//  Created by Sean Dunford on 1/17/15.
+//  Copyright (c) 2015 Razeware LLC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    func intervalGreen() -> UIColor {return UIColor(red:(124.0/255.0), green:(208.0/255.0), blue:(126.0/255.0), alpha: 1.0)}
+    func workPurple() -> UIColor {return UIColor(red: (178.0/255.0), green: (124.0/255.0), blue: (209.0/255.0), alpha: 1.0)}
+    func restRed() -> UIColor {return UIColor(red: (208.0/255.0), green: (93.0/255.0), blue: (93.0/255.0), alpha: 1.0)}
+}
+
